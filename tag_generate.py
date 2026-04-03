5610aece2b3c24afbd2ae46a46f759b18f20e998
CoderAdonis313 <coderadonis@gmail.com>
2026-04-03 19:40:31 -0400

    adding new timestamp logic

--- tag_generate.py
+++ tag_generate.py
@@ -1,9 +1,10 @@
 import cv2
 from random import randint
-from datetime import datetime
+from time import time
 
 MARKER_DICT = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_APRILTAG_36h11)
-TAG_ID = randint(0, 10)
+#TAG_ID = randint(0, 10)
+TAG_ID = 7
 SIZE_PX = 600
 
 img = cv2.aruco.generateImageMarker(MARKER_DICT, TAG_ID, SIZE_PX)
@@ -13,7 +14,7 @@ cv2.imshow('MARKER_WINDOW', img)
 cv2.waitKey(0)
 cv2.destroyWindow('MARKER_WINDOW')
 
-timestamp = datetime.now().strftime('%H_%M_%S')
-TAG_NAME = f'tag_{timestamp}.png'
+timestamp = str(int(time()*10))
+TAG_NAME = f'tag_{TAG_ID}_{timestamp}.png'
 cv2.imwrite(TAG_NAME, img)
 print('Tag generated', TAG_NAME)
